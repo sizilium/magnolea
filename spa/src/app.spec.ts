c8b358a05ab97a3720376328c44c5e6d99bbcbb2
Rolf Englputzeder <rolfenglputzeder@bbv.ch>
2019-06-06 12:09:20 +0200

    Update aurelia

--- spa/src/app.spec.ts
+++ spa/src/app.spec.ts
@@ -1,7 +1,26 @@
-import {App} from './app';
+import {bootstrap} from 'aurelia-bootstrapper';
+import {StageComponent} from 'aurelia-testing';
+import {PLATFORM} from 'aurelia-pal';
 
-describe('the app', () => {
-  it('says hello', () => {
-    expect(new App().title).toBe('Magnolea Home-Automation');
+describe('Stage App Component', () => {
+  let component;
+
+  beforeEach(() => {
+    component = StageComponent
+      .withResources(PLATFORM.moduleName('app'))
+      .inView('<app></app>');
+  });
+
+  afterEach(() => component.dispose());
+
+  it('should render message', done => {
+    component.create(bootstrap).then(() => {
+      const view = component.element;
+      expect(view.textContent.trim()).toBe('Magnolea Home-Automation');
+      done();
+    }).catch(e => {
+      fail(e);
+      done();
+    });
   });
 });
