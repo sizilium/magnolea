75d94bd5a8d6ea320036a9528bc345b2049080f5
sizilium <sizilium@googlemail.com>
2020-03-07 20:56:15 +0100

    add CORS, better REST API, added GPIO status

--- switch-service/switch-service.py
+++ switch-service/switch-service.py
@@ -8,7 +8,9 @@ import time
 
 from time import sleep
 from flask import Flask, abort, jsonify
+from flask_cors import CORS
 
+# key=name value=port
 GPIO_mappings = {
     'EG-1': 32,
     'EG-2': 18,
@@ -20,29 +22,40 @@ GPIO_mappings = {
     'OG-4': 36,
     }
 
+GPIO_states = {} # key=port value=0|1 (on|off)
 GPIO.setmode(GPIO.BOARD)
-for key, value in GPIO_mappings.iteritems():
+for key, value in GPIO_mappings.items():
     print("initialize GPIO " + key + " - " + str(value))
     GPIO.setup(value, GPIO.OUT)
+    GPIO_states[value] = 0
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
     return "Relay Control"
 
-@app.route('/switch-on/<string:id>', methods=['POST'])
+@app.route('/switch', methods=['GET'])
+def switch_status():
+    return jsonify(GPIO_states)
+
+@app.route('/switch/<string:id>/on', methods=['POST'])
 def switch_on(id):
     if id in GPIO_mappings:
-        GPIO.output(GPIO_mappings[id], False)
+        port = GPIO_mappings[id]
+        GPIO.output(port, False)
+        GPIO_states[port] = 1
         return jsonify({'id': id})
 
     abort(404) # not found
 
-@app.route('/switch-off/<string:id>', methods=['POST'])
+@app.route('/switch/<string:id>/off', methods=['POST'])
 def switch_off(id):
     if id in GPIO_mappings:
-        GPIO.output(GPIO_mappings[id], True)
+        port = GPIO_mappings[id]
+        GPIO.output(port, True)
+        GPIO_states[port] = 0
         return jsonify({'id': id})
 
     abort(400)
